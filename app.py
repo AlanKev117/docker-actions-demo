188eb497f2bd0bb00febc472c464c5db67568cb5
Alan Fuentes <alan.fuentes@wizeline.com>
2022-06-15 13:59:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 name = "Alan"
 os = "Mac OS"
-print(f"Hello, my name is {name} and I developed this app in {os}, but you can run it everywhere :)")
+print(f"Hi! My name is {name} and I developed this app in {os}, but you can run it everywhere :)")
